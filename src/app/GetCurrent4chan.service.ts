89cfd1e1ebc4aa51892ffd7af8f5259580eb0b06
Nicolai Larsen <nalar94@gmail.com>
2017-11-12 02:30:26 +0100

    d

--- src/app/GetCurrent4chan.service.ts
+++ src/app/GetCurrent4chan.service.ts
@@ -1,18 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { Injectable }     from '@angular/core';
-import { Http,Headers } from '@angular/http';
+import { Http,Headers,RequestOptions } from '@angular/http';
 @Injectable()
 export class GetCurrent4chanService {
   Stuff:any;
 
   constructor(private http: Http) {
     // Make the HTTP request:
-    const headers = new Headers({'Access-Control-Allow-Origin':"*"})
-    this.http.get('http://a.4cdn.org/gif/catalog.json').subscribe(res => {
+    const headers = new Headers();
+        headers.append('Accept', 'application/json');
+        headers.append('Access-Control-Allow-Origin', '*');
+        headers.append('Access-Control-Allow-Headers', '*');
+
+    let options = new RequestOptions({ headers: headers });
+    this.http.get('http://a.4cdn.org/gif/catalog.json',options).subscribe(res => {
     this.Stuff = res.json();
     });
-  }
-  returnStuff():any{
-    return this.Stuff;
   }
 }
--- src/app/category-list/category-list.component.ts
+++ src/app/category-list/category-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Category } from '../models/category'
 import { CategoryService } from '../category.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { RequestOptions } from '@angular/http';
+
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
@@ -11,12 +13,23 @@ export class CategoryListComponent implements OnInit {
 categories:Category[];
 Pages:Page[];
 Stuff:any;
+Threads:any = [];
   constructor(private categoryService: CategoryService, private http:HttpClient) { }
 
   ngOnInit() {
     // Make the HTTP request:
     this.http.get<Page[]>("http://a.4cdn.org/gif/catalog.json").subscribe(res => {
     this.Pages =  res;
+    for (let i = 0; i < this.Pages.length; i++) {
+
+        for (let s = 0; s < this.Pages[i].threads.length; s++) {
+            this.Threads.push(this.Pages[i].threads[s]);
+        }
+    }
+    this.Threads = this.Threads.sort(function(a, b){
+    return b.replies-a.replies
+    })
+    console.log(this.Threads);
     console.log(this.Pages[0])
     });
     this.categories = this.categoryService.getCategories();
